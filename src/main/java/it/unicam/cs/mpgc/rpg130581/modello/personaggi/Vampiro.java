524acdb4cfecfaa2363a2cf5a55203d1dddc8d5c
10NE-W0LF <maxiabazi@gmail.com>
2026-06-27 14:18:42 +0200

    Bilanciamento forza vampiri

--- src/main/java/it/unicam/cs/mpgc/rpg130581/modello/personaggi/Vampiro.java
+++ src/main/java/it/unicam/cs/mpgc/rpg130581/modello/personaggi/Vampiro.java
@@ -5,26 +5,36 @@ public class Vampiro extends Personaggio {
     private final TipoVampiro tipo;
 
     public Vampiro(TipoVampiro tipo, int livello) {
-        super(livello, calcolaVitaMassima(tipo, livello));
+        super(livello, calcolaVitaMassima(tipo, livello), calcolaForza(tipo, livello), calcolaSalute(tipo, livello));
         this.tipo = tipo;
     }
 
     private static int calcolaVitaMassima(TipoVampiro tipo, int livello) {
-        int bonusElite = tipo == TipoVampiro.VAMPIRO_ELITE ? 35 : 0;
-        int bonusBoss = tipo == TipoVampiro.ARCIDUCA ? 120 : 0;
-        return 34 + livello * 10 + bonusElite + bonusBoss;
+        int bonusElite = tipo == TipoVampiro.vampiroElite ? 18 : 0;
+        int bonusBoss = tipo == TipoVampiro.arciduca ? 160 : 0;
+        return 42 + calcolaSalute(tipo, livello) * 12 + bonusElite + bonusBoss;
+    }
+
+    private static int calcolaForza(TipoVampiro tipo, int livello) {
+        int bonusElite = tipo == TipoVampiro.vampiroElite ? 4 : 0;
+        int bonusBoss = tipo == TipoVampiro.arciduca ? 9 : 0;
+        return livello + bonusElite + bonusBoss;
+    }
+
+    private static int calcolaSalute(TipoVampiro tipo, int livello) {
+        int bonusElite = tipo == TipoVampiro.vampiroElite ? 5 : 0;
+        int bonusBoss = tipo == TipoVampiro.arciduca ? 18 : 0;
+        return livello + bonusElite + bonusBoss;
     }
 
     @Override
     public int getPotenzaAttacco() {
-        int bonusElite = tipo == TipoVampiro.VAMPIRO_ELITE ? 7 : 0;
-        int bonusBoss = tipo == TipoVampiro.ARCIDUCA ? 15 : 0;
-        return 5 + getLivello() * 3 + bonusElite + bonusBoss;
+        return 6 + getForza() * 4;
     }
 
     public int getRicompensaEsperienza() {
-        int bonusElite = tipo == TipoVampiro.VAMPIRO_ELITE ? 35 : 0;
-        int bonusBoss = tipo == TipoVampiro.ARCIDUCA ? 500 : 0;
+        int bonusElite = tipo == TipoVampiro.vampiroElite ? 35 : 0;
+        int bonusBoss = tipo == TipoVampiro.arciduca ? 500 : 0;
         return 22 + getLivello() * 13 + bonusElite + bonusBoss;
     }
 
